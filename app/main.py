d726d0c07a6c6e80e4dac746f37f5af3f93785aa
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-09 11:51:41 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, source_file, target_file = parts
+
+    # Do nothing if file names are the same
+    if source_file == target_file:
+        return
+
+    with open(source_file, "r") as file_in, open(target_file, "w") as file_out:
+        file_out.write(file_in.read())
